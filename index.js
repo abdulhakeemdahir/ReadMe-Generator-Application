83509c02bf6e75dd02acae8591034bb330b068cc
Abdulhakeem Dahir <somlutions@gmail.com>
2020-12-07 15:08:38 -0800

    setting up inquirer prompt questions

--- index.js
+++ index.js
@@ -3,13 +3,71 @@ const fs = require("fs");
 const util = require("./assets/utils/generateMarkdown");
 
 // array of questions for user
-const questions = [];
-
+const questions = () =>
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the application name?",
+    },
+    {
+      type: "input",
+      name: "comment",
+      message: "Developer Comment?",
+    },
+    {
+      type: "input",
+      name: "developer",
+      message: "Developer Request?",
+    },
+    {
+      type: "input",
+      name: "story",
+      message: "User Story?",
+    },
+    {
+      type: "input",
+      name: "criteria",
+      message: "Acceptance Criteria?",
+    },
+    {
+      type: "input",
+      name: "Mockup",
+      message: "Any Mockup?",
+    },
+    {
+      type: "input",
+      name: "features",
+      message: "Features and Highlights?",
+    },
+    {
+      type: "input",
+      name: "live",
+      message: "Video Link?",
+    },
+    {
+      type: "input",
+      name: "install",
+      message: "Installation and Git Repo?",
+    },
+    {
+      type: "input",
+      name: "contributors",
+      message: "Any Contributors?",
+    },
+    {
+      type: "input",
+      name: "license",
+      message: "What's the license?",
+    },
+  ]);
 // function to write README file
 function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {}
+function init() {
+  questions();
+}
 
 // function call to initialize program
 init();
